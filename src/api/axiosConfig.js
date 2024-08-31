70f08e76ac9a56317340578e379835a224fecb44
Nova Ashokan <nova.ashokan@gmail.com>
2024-08-31 18:02:19 +0400

    auth

---
+++ src/api/axiosConfig.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import store from "../store";
+
+const instance = axios.create({
+  baseURL: "http://localhost:8080/users",
+});
+
+instance.interceptors.request.use(
+  (config) => {
+    const state = store.getState();
+    const sessionId = state.auth.sessionId;
+
+    if (sessionId) {
+      config.headers["Session-ID"] = sessionId;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
